5ce489ae5c738856e8651759ae13e88b0669d9c4
keqinhuang <3102243168@qq.com>
2025-02-14 12:44:31 +0800

    前端传递短链接有效期时间序列化

--- project/src/main/java/com/huanghkeqin/shortlink/project/dto/req/ShortLinkCreateReqDTO.java
+++ project/src/main/java/com/huanghkeqin/shortlink/project/dto/req/ShortLinkCreateReqDTO.java
@@ -1,5 +1,6 @@
 package com.huanghkeqin.shortlink.project.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -9,11 +10,33 @@ import java.util.Date;
  */
 @Data
 public class ShortLinkCreateReqDTO {
-    private String domain; // 域名
-    private String originUrl; // 原始链接
-    private String gid; // 分组标识
-    private Integer createdType; // 创建类型 1：控制台 0：接口
-    private Integer  validDateType; // 有效期类型 0：永久有效 1：用户自定义
-    private Date validDate; // 有效期
-    private String describe; // 描述
+    /**
+     * 域名
+     */
+    private String domain;
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+    /**
+     * 分组标识
+     */
+    private String gid;
+    /**
+     *  创建类型 1：控制台 0：接口
+     */
+    private Integer createdType;
+    /**
+     * 有效期类型 0：永久有效 1：用户自定义
+     */
+    private Integer  validDateType;
+    /**
+     * 有效期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date validDate;
+    /**
+     * 描述
+     */
+    private String describe;
 }
