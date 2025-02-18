410b23150a19a922dc7e25190268385813be2bed
keqinhuang <3102243168@qq.com>
2025-02-18 17:59:48 +0800

    feature:开发获取通过原始链接url获取网站标题接口

---
+++ admin/src/main/java/com/huangkeqin/shortlink/admin/controller/UrlTitleController.java
@@ -0,0 +1,33 @@
+package com.huangkeqin.shortlink.admin.controller;
+
+
+import com.huangkeqin.shortlink.admin.common.convention.result.Result;
+import com.huangkeqin.shortlink.admin.common.convention.result.Results;
+import com.huangkeqin.shortlink.admin.remote.ShortLinkRemoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * URL 标题控制层
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class UrlTitleController {
+
+    /**
+     * 后续重构为 Feign调用
+     */
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+    };
+    /**
+     * 根据 URL 获取对应网站的标题
+     */
+    @GetMapping("/api/short-link/admin/v1/title")
+    public Result<String> getTitleByUrl(@RequestParam("url") String url) {
+        return shortLinkRemoteService.getTitleByUrl(url);
+    }
+
+}
