34dea5c57c79f2dfa0fa4ef2f3452976e991627f
keqinhuang <3102243168@qq.com>
2025-02-20 17:17:46 +0800

    修改错误

--- admin/src/main/java/com/huangkeqin/shortlink/admin/config/DataBaseConfiguration.java
+++ admin/src/main/java/com/huangkeqin/shortlink/admin/config/DataBaseConfiguration.java
@@ -1,4 +1,4 @@
-package com.huanghkeqin.shortlink.project.config;
+package com.huangkeqin.shortlink.admin.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
