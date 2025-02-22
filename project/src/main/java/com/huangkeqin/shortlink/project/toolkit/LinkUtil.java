6ba4314fe61f28d9c0a29d7bae60b09941bba36f
keqinhuang <3102243168@qq.com>
2025-02-22 18:51:21 +0800

    feature: 开发短链接监控之基础访问uip数据

--- project/src/main/java/com/huangkeqin/shortlink/project/toolkit/LinkUtil.java
+++ project/src/main/java/com/huangkeqin/shortlink/project/toolkit/LinkUtil.java
@@ -2,23 +2,54 @@ package com.huangkeqin.shortlink.project.toolkit;
 
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Date;
 import java.util.Optional;
+
 import static com.huangkeqin.shortlink.project.common.constant.ShortLinkConstant.DEFAULT_CACHE_VALID_TIME;
+
 /**
  * 短链接工具类
  */
 public class LinkUtil {
     /**
      * 获取短链接有效期
+     *
      * @param validDate
      * @return
      */
-    public static long getLinkCacheValidDate(Date validDate){
+    public static long getLinkCacheValidDate(Date validDate) {
         return Optional.ofNullable(validDate).
                 map(each -> DateUtil.between(new Date(), each, DateUnit.MS))
                 .orElse(DEFAULT_CACHE_VALID_TIME);
 
     }
+
+    /**
+     * 获取用户真实IP
+     *
+     * @param request 请求
+     * @return 用户真实IP
+     */
+    public static String getActualIp(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+        return ipAddress;
+    }
+
 }
