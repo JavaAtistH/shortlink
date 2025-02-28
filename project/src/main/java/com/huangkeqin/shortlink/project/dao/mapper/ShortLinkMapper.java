3d96745fe901d6686ceaa1da056724362c78f9f6
keqinhuang <3102243168@qq.com>
2025-02-28 14:30:03 +0800

    refactor :重构短链接监控使用Redis Stream消息队列，避免请求直接存储MySQL

--- project/src/main/java/com/huangkeqin/shortlink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/com/huangkeqin/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -1,10 +1,35 @@
+
 package com.huangkeqin.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.huangkeqin.shortlink.project.dao.entity.ShortLinkDO;
+import com.huangkeqin.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import com.huangkeqin.shortlink.project.dto.req.ShortLinkRecycleBinPageReqDTO;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 短链接持久层
+ * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
  */
 public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+
+    /**
+     * 短链接访问统计自增
+     */
+    void incrementStats(@Param("gid") String gid,
+                        @Param("fullShortUrl") String fullShortUrl,
+                        @Param("totalPv") Integer totalPv,
+                        @Param("totalUv") Integer totalUv,
+                        @Param("totalUip") Integer totalUip);
+
+    /**
+     * 分页统计短链接
+     */
+    IPage<ShortLinkDO> pageLink(ShortLinkPageReqDTO requestParam);
+
+    /**
+     * 分页统计回收站短链接
+     */
+    IPage<ShortLinkDO> pageRecycleBinLink(ShortLinkRecycleBinPageReqDTO requestParam);
 }
